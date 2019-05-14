ed00611191fefd4c5777c55159e37935d6b818e5
Sebreiro <admin@local.local>
2019-05-15 01:30:23 +0300

    Add ModelValidation attribute

---
+++ src/TelegramBotMessageSender.Api/Attributes/ModelValidationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TelegramBotMessageSender.WebApi.Attributes
+{
+    public class ModelValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(new ValidationProblemDetails(context.ModelState));
+            }
+        }
+    }
+}
